2548f73bea03093d8e9f2b9b7a37f024b6e7ee07
Ji Xu <ji@motivodata.com>
2019-03-05 13:58:01 -0800

    [SERVER] `setup_learner` update to use `load_learner`.

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 model_file_url = 'https://drive.google.com/uc?export=download&id=1g758A2NMZaTWIyXtl7JaAKMGu7SIQIuJ'
-model_file_name = 'model'
+model_file_name = 'export.pkl'
 classes = ['sisley','pissarro','monet','degas','manet','renoir']
 path = Path(__file__).parent
 
@@ -25,12 +25,17 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        tfms=get_transforms(), size=224).normalize(imagenet_stats)
-    learn = create_cnn(data_bunch, models.resnet34, pretrained=False)
-    learn.load(model_file_name)
-    return learn
+    await download_file(model_file_url, path/'models'/model_file_name)
+    try:
+        learn = load_learner(path / 'models')
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
